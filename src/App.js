56080297ecacecb87552339423cebce37360ad5f
MahmoudAliKhider <mahmoudkhider37@gmail.com>
2022-04-21 03:27:00 +0200

    react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './coms/Footer';
+import LeftMain from './coms/LeftMain';
+
+function App() {
+  return (
+    <div className="App">
+      <LeftMain />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
